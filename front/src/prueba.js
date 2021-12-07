3acf16901fbc8edc3e96e6e873448e305aad4077
AlondraSanchez <alondra.8418@gmail.com>
2021-12-07 15:48:08 -0600

    prueba server node

--- front/src/prueba.js
+++ front/src/prueba.js
@@ -1,1 +1,6 @@
-var x = 1;
+const http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Howola');
+}).listen(20000);
