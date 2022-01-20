87a0b4fc95a0b6cd4fbaaba88f86f668b6ef572a
kevuriel <kevuriel@hotmail.com>
2022-01-19 19:19:33 -0600

    Cambio en el padding y nombres de variables

--- server/services/Rsa.js
+++ server/services/Rsa.js
@@ -1,31 +1,62 @@
-const crypto = require("crypto");
+/**
+ * @fileoverview Rsa, clase que contiene los metodos de cifrado y descifrado de rsa
+ * @version 1.1
+ * @author Kevin Uriel Malvaez Landeros
+ * history
+ * v.1.0 primera implementacion de las funciones cifrar y descifrar las
+ * v.1.1 cambio en el padding a RSA_PKCS1_PADDING y variables a español
+ */
 
+const crypto = require("crypto");// Modulo crypto, necesario para usar rsa
+
+/** @constructor */
 const Rsa = function (rsa) {
-  this.publicKey = rsa.publicKey;
-  this.privateKey = rsa.privateKey;
+  /** 
+   * Propiedad que indica la llave publica
+   * @type {string}
+  */
+  this.llavePublica = rsa.llavePublica;
+  /** 
+   * Propiedad que indica la llave privada
+   * @type {string}
+  */
+  this.llavePrivada = rsa.llavePrivada;
 };
 
-Rsa.cifrar = function (publicKey, data) {
-  const encryptedData = crypto.publicEncrypt(
+/**
+ * Cifra los datos con llave publica
+ * @param llavePublica {string} 
+ * @param datos {string} 
+ * @return datosCifrados {string} 
+ */
+Rsa.cifrar = function (llavePublica, datos) {
+  const datosCifrados = crypto.publicEncrypt(
     {
-      key: publicKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      key: llavePublica,
+      padding: crypto.constants.RSA_PKCS1_PADDING
     },
-    Buffer.from(data)
+    Buffer.from(datos)
   );
-  return encryptedData.toString("base64");
+  return datosCifrados.toString("base64");
 };
 
-Rsa.descifrar = function (privateKey, data, pass) {
-  const decryptedData = crypto.privateDecrypt(
+/**
+ * Descifra los datos con la llave privada y la contraseña asociada a esta
+ * @param llavePrivada {string} 
+ * @param datos {string} 
+ * @param contrasena {string}
+ * @return datosDescifrados {string}  
+ */
+Rsa.descifrar = function (llavePrivada, datos, contrasena) {
+  const datosDescifrados = crypto.privateDecrypt(
     {
-      key: privateKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      passphrase: pass,
+      key: llavePrivada,
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+      passphrase: contrasena,
     },
-    Buffer.from(data, "base64")
+    Buffer.from(datos, "base64")
   );
-  return decryptedData.toString();
+  return datosDescifrados.toString();
 };
 
 module.exports = Rsa;
