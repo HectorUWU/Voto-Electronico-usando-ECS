429434c676bc151dc98098a0d696934820858718
AlondraSanchez <alondra.8418@gmail.com>
2022-01-20 14:20:17 -0600

    Código documentado y pruebas unitarias para ECS
    Co-authored-by: HectorUWU <HectorUWU@users.noreply.github.com>

--- server/services/ECS.js
+++ server/services/ECS.js
@@ -1,9 +1,25 @@
-const rref = require('rref')
+/** 
+ * @fileoverview ECS, Clase que contiene los métodos del esquema de compartición de secretos de Shamir
+ * @version 1.0
+ * @author Alondra Jacqueline Jacinto Sánchez
+ * history 
+ * v.1.0 Se crean funciones fragmentarSecreto, evaluarPolinomio, obtenerNumeroAleatorio, desfragmentarSecreto, generarEcuacion y resolverSistemaEcuaciones
+ */
+const rref = require("rref"); // Módulo rref, necesario para resolver el sistema de ecuaciones 
 
-const CAMPO_TAM = Math.pow(10, 2);
+const CAMPO_TAM = Math.pow(10, 2); // Constante que define el tamaño del plano
+/** @constructor */
+const ECS = function() {}
 
-// u = umbral; p = no. de participantes
-function fragmentarSecreto(secreto, u, p) {
+
+/** 
+ * Fragmenta un secreto utilizando ECS de Shamir
+ * @param secreto {number} 
+ * @param u {number}, umbral
+ * @param p {number}, No. de participantes
+ * @return fragmentos {map}
+ */
+ECS.fragmentarSecreto = function(secreto, u, p) {
     const coeficientes = []
     for (let i = 1; i < u; i++) {
         coeficientes.push(obtenerNumeroAleatorio(0, CAMPO_TAM))
@@ -14,13 +30,19 @@ function fragmentarSecreto(secreto, u, p) {
     for (let i = 0; i < p; i++) {
         const x = obtenerNumeroAleatorio(1, CAMPO_TAM)
 
-        fragmentos.set(x, evaluarPolinomio(x, coeficientes))
+        fragmentos.set(x, ECS.evaluarPolinomio(x, coeficientes))
     }
 
     return fragmentos
 }
 
-function evaluarPolinomio(x, coeficientes) {
+/** 
+ * Evalua un polinomio en x
+ * @param x {number}, valor de x a evaluar 
+ * @param coeficientes {number array}, arreglo con los coeficientes del polinomio
+ * @return evaluacion {number}
+ */
+ECS.evaluarPolinomio = function(x, coeficientes) {
     let evaluacion = 0
     let j = 0
     for (let i = coeficientes.length - 1; i >= 0; i--) {
@@ -37,42 +59,46 @@ function obtenerNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function desfragmentarSecreto(fragmentos) {
+/** 
+ * Recupera un secreto con base en los fragmentos utilizando ECS de Shamir
+ * @param fragmentos {map}
+ * @return ECS.resolverSistemaEcuaciones {number}, secreto desfragmentado
+ */
+ECS.desfragmentarSecreto = function(fragmentos) {
     const ecuaciones = []
     let i = 0;
     for (const [key, value] of fragmentos) {
-        ecuaciones[i] = generarEcuacion(key, value, fragmentos.size)
+        ecuaciones[i] = ECS.generarEcuacion(key, value, fragmentos.size)
         i++
     }
-    return resolverSistemaEcuaciones(ecuaciones)
+    return ECS.resolverSistemaEcuaciones(ecuaciones)
 }
 
-function generarEcuacion(x, y, grado) {
+/** 
+ * Genera una ecuación única para un punto dado
+ * @param x {number}, coordenada en x 
+ * @param y {number}, coordenada en y
+ * @param u {number}, umbral del ECS de Shamir
+ * @return ecuacion {number array}
+ */
+ECS.generarEcuacion = function(x, y, u) {
     const ecuacion = []
-    for (let i = 0; i < grado; i++) {
+    for (let i = 0; i < u; i++) {
         ecuacion.push(Math.pow(x, i))
     }
     ecuacion.push(y)
     return ecuacion
 }
 
-function resolverSistemaEcuaciones(ecuaciones) {
+/** 
+ * Utiliza el módulo rref para resolver un sistema de ecuaciones mediante eliminación Gaussiana
+ * @param ecuaciones {number matrix}, Matriz con los coeficientes del sistema de ecuaciones
+ * @return x0[res[0].length - 1] {number}, Valor de x0
+ */
+ECS.resolverSistemaEcuaciones = function(ecuaciones) {
     const res = rref(ecuaciones)
     const x0 = res[0]
     return x0[res[0].length - 1]
 }
 
-const u = 5
-const p = 10
-const fragmentos = fragmentarSecreto(12345, u, p)
-const muestra = new Map()
-for (const [key, value] of fragmentos) {
-    muestra.set(key, value)
-    if (muestra.size === u)
-        break
-}
-const desfragmentacion = desfragmentarSecreto(muestra)
-
-console.log(fragmentos)
-console.log(muestra)
-console.log(desfragmentacion)
+module.exports = ECS;
