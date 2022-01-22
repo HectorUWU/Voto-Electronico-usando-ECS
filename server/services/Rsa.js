6ba7eafe28ff57e53f0a4cbb2fd28200e40fc5de
HectorUWU <perezbarajashector@gmail.com>
2022-01-22 15:19:30 -0600

    cambiando definicion de clases

--- server/services/Rsa.js
+++ server/services/Rsa.js
@@ -8,55 +8,68 @@
  */
 
 const crypto = require("crypto");// Modulo crypto, necesario para usar rsa
+class Rsa {
+  constructor (llave) {
+    /** 
+     * Propiedad que indica la llave publica o privada.
+     * @type {string}
+    */
+    this.llave= llave;
+  };
 
-/** @constructor */
-const Rsa = function (rsa) {
-  /** 
-   * Propiedad que indica la llave publica
-   * @type {string}
-  */
-  this.llavePublica = rsa.llavePublica;
-  /** 
-   * Propiedad que indica la llave privada
-   * @type {string}
-  */
-  this.llavePrivada = rsa.llavePrivada;
-};
+  /**
+   * Cifra los datos con llave publica
+   * @param datos {string} 
+   * @return datosCifrados {string} 
+   */
+  cifrar = function (datos) {
+    const datosCifrados = crypto.publicEncrypt(
+      {
+        key: this.llave,
+        padding: crypto.constants.RSA_PKCS1_PADDING
+      },
+      Buffer.from(datos)
+    );
+    return datosCifrados.toString("base64");
+  };
 
-/**
- * Cifra los datos con llave publica
- * @param llavePublica {string} 
- * @param datos {string} 
- * @return datosCifrados {string} 
- */
-Rsa.cifrar = function (llavePublica, datos) {
-  const datosCifrados = crypto.publicEncrypt(
-    {
-      key: llavePublica,
-      padding: crypto.constants.RSA_PKCS1_PADDING
-    },
-    Buffer.from(datos)
-  );
-  return datosCifrados.toString("base64");
-};
+  /**
+   * Descifra los datos con la llave privada y la contraseña asociada a esta 
+   * @param datos {string} 
+   * @param contrasena {string}
+   * @return datosDescifrados {string}  
+   */
+  descifrar = function (datos, contrasena) {
+    const datosDescifrados = crypto.privateDecrypt(
+      {
+        key: this.llave,
+        padding: crypto.constants.RSA_PKCS1_PADDING,
+        passphrase: contrasena,
+      },
+      Buffer.from(datos, "base64")
+    );
+    return datosDescifrados.toString();
+  };
 
-/**
- * Descifra los datos con la llave privada y la contraseña asociada a esta
- * @param llavePrivada {string} 
- * @param datos {string} 
- * @param contrasena {string}
- * @return datosDescifrados {string}  
- */
-Rsa.descifrar = function (llavePrivada, datos, contrasena) {
-  const datosDescifrados = crypto.privateDecrypt(
-    {
-      key: llavePrivada,
-      padding: crypto.constants.RSA_PKCS1_PADDING,
-      passphrase: contrasena,
-    },
-    Buffer.from(datos, "base64")
-  );
-  return datosDescifrados.toString();
-};
-
+  static generarLLaves(contrasena) {
+    const {
+      publicKey,
+      privateKey,
+    } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'pkcs1',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: contrasena
+      }
+    
+    });
+    return ([publicKey, privateKey])
+  }
+}
 module.exports = Rsa;
