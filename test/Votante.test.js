667b1c365a944b424604803f06fee6320c834d81
kevuriel <kevuriel@hotmail.com>
2022-03-24 15:56:19 -0600

    Arreglando tests

--- test/Votante.test.js
+++ test/Votante.test.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-undef */
-const Votante = require('../server/services/Votante.js')
+require("iconv-lite").encodingExists("foo");
+const Votante = require("../server/services/Votante.js");
 
-const votanteSinVotar = new Votante(false, true)
-test('Votar sin haber votado previamente y estado academico activo', () => {
-    expect(votanteSinVotar.votar(2,3,2)).resolves.toBe({mensaje: "Voto correcto"})
-})
+const votanteSinVotar = new Votante(0, 1);
+test("Votar sin haber votado previamente y estado academico activo", () => {
+  expect(votanteSinVotar.votar(2, 3, 2)).resolves.toBe({
+    mensaje: "Voto correcto",
+  });
+});
 
-const votanteConVotoPrevio = new Votante(true, true)
-test('Votar sin haber votado previamente y estado academico activo', () => {
-    expect(votanteConVotoPrevio.votar(2,3,2)).rejects.toMatch("Ya has ejercido tu voto")
-})
+const votanteConVotoPrevio = new Votante(1, 1);
+test("Votar sin haber votado previamente y estado academico activo", () => {
+  expect(votanteConVotoPrevio.votar(2, 3, 2)).rejects.toThrow(
+    "Ya has ejercido tu voto"
+  );
+});
 
-const votanteSinEstadoAcademico = new Votante(false, false)
-test('Votar sin haber votado previamente y estado academico activo', () => {
-    expect(votanteSinEstadoAcademico.votar(2,3,2)).rejects.toMatch("No estas inscrito y no puedes votar")
-})
+const votanteSinEstadoAcademico = new Votante(0, 0);
+test("Votar sin haber votado previamente y estado academico activo", () => {
+  expect(votanteSinEstadoAcademico.votar(2, 3, 2)).rejects.toThrow(
+    "No estas inscrito y no puedes votar"
+  );
+});
