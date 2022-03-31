ffd619386aea10c66d383397ad8dba90c5afe734
HectorUWU <perezbarajashector@gmail.com>
2022-03-31 15:37:52 -0600

    cambiando a hash route

--- front/src/Main.js
+++ front/src/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import SingIn from "./components/SingIn";
