dc5daa9bebe6f01500681350e42daa21e8e51272
AlondraSanchez <alondra.8418@gmail.com>
2022-04-04 17:52:51 -0500

    Asignacion de permisos Oauth

--- server/services/Correo.js
+++ server/services/Correo.js
@@ -7,7 +7,7 @@
  * v.1.0 primera version del archivo
  */
 
-const nodemailer = require("nodemailer"); // modulo nodemailer, necesario para el envio de correo
+const nodemailer = require("nodemailer/lib/smtp-connection"); // modulo nodemailer, necesario para el envio de correo
 /**  @class */
 class Correo {
   /** @constructor */
@@ -24,10 +24,10 @@ class Correo {
           "568226944879-66clq91ffs7l0jogurqmastd0jh89o5n.apps.googleusercontent.com",
         clientSecret: "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",
         refreshToken:
-          "1//0fNsWL7OmkFI2CgYIARAAGA8SNwF-L9IrTxnGnPEe3g9MEWV39TIGVI-YdpOxMErQ-kredwfcVlLE-gDh--_EpQWCANXDS21dDYY",
+          "1//0flTrB-PYiOvsCgYIARAAGA8SNwF-L9IrjG6oYn0uRqcF0kWHV-xw0gDxYxMbYZhnu8Fc6rYvXpDFrg2n19bZLgzfjSqf2_KCtd0",
         accessToken:
-          "ya29.A0ARrdaM_3ohsYetZElzSWUcfcxwydsrv6TupvDQD4tYBY7GfgmwQ4sil6z9TECDuvpflu8FHpBxC45g7EMu1fW20czEzytVZYr1iCYpW4kuaDwmJHR71E0H2PjpQi3NkMteqqDLfDL3UR4CTVtNQc6M1VRu3k",
-        timeout: 1649110967783 - Date.now(),
+          "ya29.A0ARrdaM_346vzKhxZDbGAH_ekKDO58y0ggv0Q3AMEIuh0ym8bVG3DyYApHW04k40UrgCW8grFlgzoAozgCSiMNzb-rzbv0HfDZScMzmV-0pJtHx_FvfTzh95_d-yH7dy7JWjZ1qW-QEFCZpaH9oTaHqjxAjEi",
+        timeout: 1649116253973 - Date.now(),
       },
     });
   }
