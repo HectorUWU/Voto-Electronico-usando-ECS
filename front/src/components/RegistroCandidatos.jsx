22867983559b6c75730f6d737760e6c94ef72776
HectorUWU <perezbarajashector@gmail.com>
2022-04-05 14:32:23 -0500

    Revirtiendo cambios

--- front/src/components/RegistroCandidatos.jsx
+++ front/src/components/RegistroCandidatos.jsx
@@ -105,12 +105,11 @@ export default function RegistroCandidatos() {
                   Accept: "application/json",
                   "Content-Type": "application/json",
                   "auth-token": data.token,
-                  Authorization: 'Client-ID 457be7e03ae9a71'
                 },
-                body: files,
+                body: JSON.stringify(datos),
               };
               setOpenCargando(true);
-              fetch("https://api.imgur.com/3/image", config)
+              fetch("https://vota-escom.herokuapp.com/api/subir", config)
                 .then((response) => response.json())
                 .then((response) => {
                   setOpenCargando(false);
@@ -118,7 +117,7 @@ export default function RegistroCandidatos() {
                     setError(response.error);
                     setShowError(true);
                   } else {
-                    console.log(response.data.link);
+                    setOpen(true);
                   }
                 });
             }
