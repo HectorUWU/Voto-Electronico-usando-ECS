7cdd7567556b59056052bcd73101ca17222afef8
HectorUWU <perezbarajashector@gmail.com>
2022-05-12 18:06:38 -0500

    Arreglando coneccion con la base de datos.

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const conexion = mysql.createPool({
-  host:  "localhost",
-  user: "root",
-  password: "root",
-  database: "votoe",
+  host:  process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   timezone: '-06:00',
   waitForConnections: true,
   connectionLimit: 10,
