c36cef6e12abca8fc2f776264cd1602cdacb23d1
AlondraSanchez <alondra.8418@gmail.com>
2022-05-17 10:47:21 -0500

    Cambios visuales a publicacion de resultados

--- front/src/components/Resultados.jsx
+++ front/src/components/Resultados.jsx
@@ -57,7 +57,7 @@ export default function Resultados() {
           setMensaje(response.error);
           setOpen(true);
         } else {
-          console.log("Fin de proceso");
+          setMensaje(response.mensaje);
           setOpen(true);
         }
       });
