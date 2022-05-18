bdf515013c020e99ac44c95d87cb042b9e40cce1
AlondraSanchez <alondra.8418@gmail.com>
2022-05-17 20:35:39 -0500

    Se arregla bug de colisiones al fragmentar votos

--- server/services/ECS.js
+++ server/services/ECS.js
@@ -1,105 +1,108 @@
-/** 
+/**
  * @fileoverview ECS, Clase que contiene los métodos del esquema de compartición de secretos de Shamir
  * @version 1.0
  * @author Alondra Jacqueline Jacinto Sánchez
- * history 
+ * history
  * v.1.0 Se crean funciones fragmentarSecreto, evaluarPolinomio, obtenerNumeroAleatorio, desfragmentarSecreto, generarEcuacion y resolverSistemaEcuaciones
  */
-const rref = require("rref"); // Módulo rref, necesario para resolver el sistema de ecuaciones 
+const rref = require("rref"); // Módulo rref, necesario para resolver el sistema de ecuaciones
 
 const CAMPO_TAM = Math.pow(10, 2); // Constante que define el tamaño del plano
 
 class ECS {
-    /** @constructor */
-    // constructor() {}
-    /** 
-     * Fragmenta un secreto utilizando ECS de Shamir
-     * @param secreto {number} 
-     * @param u {number}, umbral
-     * @param p {number}, No. de participantes
-     * @return fragmentos {map}
-     */
-    fragmentarSecreto(secreto, u, p) {
-        const coeficientes = []
-        for (let i = 1; i < u; i++) {
-            coeficientes.push(obtenerNumeroAleatorio(0, CAMPO_TAM))
-        }
-        coeficientes.push(secreto);
-        const fragmentos = new Map()
+  /** @constructor */
+  // constructor() {}
+  /**
+   * Fragmenta un secreto utilizando ECS de Shamir
+   * @param secreto {number}
+   * @param u {number}, umbral
+   * @param p {number}, No. de participantes
+   * @return fragmentos {map}
+   */
+  fragmentarSecreto(secreto, u, p) {
+    const coeficientes = [];
+    for (let i = 1; i < u; i++) {
+      coeficientes.push(obtenerNumeroAleatorio(0, CAMPO_TAM));
+    }
+    coeficientes.push(secreto);
+    const fragmentos = new Map();
 
-        for (let i = 0; i < p; i++) {
-            const x = obtenerNumeroAleatorio(1, CAMPO_TAM)
+    for (let i = 0; i < p; i++) {
+      let x;
+      do {
+        x = obtenerNumeroAleatorio(1, CAMPO_TAM);
+      } while (fragmentos.get(x) !== undefined);
 
-            fragmentos.set(x, this.evaluarPolinomio(x, coeficientes))
-        }
-
-        return fragmentos
+      fragmentos.set(x, this.evaluarPolinomio(x, coeficientes));
     }
 
-    /** 
-     * Evalua un polinomio en x
-     * @param x {number}, valor de x a evaluar 
-     * @param coeficientes {number array}, arreglo con los coeficientes del polinomio
-     * @return evaluacion {number}
-     */
-    evaluarPolinomio(x, coeficientes) {
-        let evaluacion = 0
-        let j = 0
-        for (let i = coeficientes.length - 1; i >= 0; i--) {
-            evaluacion += Math.pow(x, i) * coeficientes[j]
-            j++
-        }
+    return fragmentos;
+  }
 
-        return evaluacion
+  /**
+   * Evalua un polinomio en x
+   * @param x {number}, valor de x a evaluar
+   * @param coeficientes {number array}, arreglo con los coeficientes del polinomio
+   * @return evaluacion {number}
+   */
+  evaluarPolinomio(x, coeficientes) {
+    let evaluacion = 0;
+    let j = 0;
+    for (let i = coeficientes.length - 1; i >= 0; i--) {
+      evaluacion += Math.pow(x, i) * coeficientes[j];
+      j++;
     }
 
-    /** 
-     * Recupera un secreto con base en los fragmentos utilizando ECS de Shamir
-     * @param fragmentos {map}
-     * @return ECS.resolverSistemaEcuaciones {number}, secreto desfragmentado
-     */
-    desfragmentarSecreto(fragmentos) {
-        const ecuaciones = []
-        let i = 0;
-        for (const [key, value] of fragmentos) {
-            ecuaciones[i] = this.generarEcuacion(key, value, fragmentos.size)
-            i++
-        }
-        return this.resolverSistemaEcuaciones(ecuaciones)
-    }
+    return evaluacion;
+  }
 
-    /** 
-     * Genera una ecuación única para un punto dado
-     * @param x {number}, coordenada en x 
-     * @param y {number}, coordenada en y
-     * @param u {number}, umbral del ECS de Shamir
-     * @return ecuacion {number array}
-     */
-    generarEcuacion(x, y, u) {
-        const ecuacion = []
-        for (let i = 0; i < u; i++) {
-            ecuacion.push(Math.pow(x, i))
-        }
-        ecuacion.push(y)
-        return ecuacion
+  /**
+   * Recupera un secreto con base en los fragmentos utilizando ECS de Shamir
+   * @param fragmentos {map}
+   * @return ECS.resolverSistemaEcuaciones {number}, secreto desfragmentado
+   */
+  desfragmentarSecreto(fragmentos) {
+    const ecuaciones = [];
+    let i = 0;
+    for (const [key, value] of fragmentos) {
+      ecuaciones[i] = this.generarEcuacion(key, value, fragmentos.size);
+      i++;
     }
+    return this.resolverSistemaEcuaciones(ecuaciones);
+  }
 
-    /** 
-     * Utiliza el módulo rref para resolver un sistema de ecuaciones mediante eliminación Gaussiana
-     * @param ecuaciones {number matrix}, Matriz con los coeficientes del sistema de ecuaciones
-     * @return x0[res[0].length - 1] {number}, Valor de x0
-     */
-    resolverSistemaEcuaciones(ecuaciones) {
-        const res = rref(ecuaciones)
-        const x0 = res[0]
-        return x0[res[0].length - 1]
+  /**
+   * Genera una ecuación única para un punto dado
+   * @param x {number}, coordenada en x
+   * @param y {number}, coordenada en y
+   * @param u {number}, umbral del ECS de Shamir
+   * @return ecuacion {number array}
+   */
+  generarEcuacion(x, y, u) {
+    const ecuacion = [];
+    for (let i = 0; i < u; i++) {
+      ecuacion.push(Math.pow(x, i));
     }
+    ecuacion.push(y);
+    return ecuacion;
+  }
+
+  /**
+   * Utiliza el módulo rref para resolver un sistema de ecuaciones mediante eliminación Gaussiana
+   * @param ecuaciones {number matrix}, Matriz con los coeficientes del sistema de ecuaciones
+   * @return x0[res[0].length - 1] {number}, Valor de x0
+   */
+  resolverSistemaEcuaciones(ecuaciones) {
+    const res = rref(ecuaciones);
+    const x0 = res[0];
+    return x0[res[0].length - 1];
+  }
 }
 
 function obtenerNumeroAleatorio(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = ECS;
