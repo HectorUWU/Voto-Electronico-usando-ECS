9ba403f14ff80b5b8b3176f772c58648f3cf259e
AlondraSanchez <alondra.8418@gmail.com>
2022-05-19 18:07:12 -0500

    Actualizacion de eleccion de candidatos ganadores

--- server/services/Votacion.js
+++ server/services/Votacion.js
@@ -1,69 +1,82 @@
-/** 
+/**
  * @fileoverview Votacion, Clase que contiene los métodos para el conteo de votos
  * @version 1.0
  * @author Alondra Jacqueline Jacinto Sánchez
- * history 
+ * history
  * v.1.0 Se crean funciones contarVotos y obtenerFrecuencia
  */
 
 class Votacion {
-    /** 
-     * Cuenta los votos y retorna resultados
-     * @param votos {number array}, arreglo con el valor del ID del candidato por el que se votó
-     * @return resultados {Candidato array}, arreglo de objetos tipo Candidato 
-     */
-    contarVotos(votos) {
-        const candidatos = new Set(votos);
-        const resultados = []
-        for (const item of candidatos) {
-            resultados.push({ id: item, votos: this.obtenerFrecuencia(votos, item) })
-                // resultados.push(new Candidato(item, this.obtenerFrecuencia(votos, item)))
+  /**
+   * Cuenta los votos y retorna resultados
+   * @param votos {number array}, arreglo con el valor del ID del candidato por el que se votó
+   * @return resultados {Candidato array}, arreglo de objetos tipo Candidato
+   */
+  contarVotos(votos) {
+    const candidatos = new Set(votos);
+    const resultados = [];
+    for (const item of candidatos) {
+      resultados.push({ id: item, votos: this.obtenerFrecuencia(votos, item) });
+      // resultados.push(new Candidato(item, this.obtenerFrecuencia(votos, item)))
+    }
+    return this.decidirCandidatosElectos(resultados);
+  }
+
+  /**
+   * Establece el estatus ganador para los candidatos electos
+   * @param resultados {Candidato array}, arreglo de Candidatos con el id y conteo de votos
+   * @return resultadosFinales {Candidato array}, arreglo de Candidatos con el id, conteo de votos y estatus (1 para electo, 0 para no electo)
+   */
+  decidirCandidatosElectos(resultados) {
+    const candidatosElegidos = 8; // Es el numero de candidatos que deben elegirse en la votación
+    const len = resultados.length;
+
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (resultados[j].votos < resultados[j + 1].votos) {
+          const temp = resultados[j];
+          resultados[j] = resultados[j + 1];
+          resultados[j + 1] = temp;
         }
-        return this.decidirCandidatosElectos(resultados)
+      }
     }
 
-    /** 
-     * Establece el estatus ganador para los candidatos electos
-     * @param resultados {Candidato array}, arreglo de Candidatos con el id y conteo de votos
-     * @return resultadosFinales {Candidato array}, arreglo de Candidatos con el id, conteo de votos y estatus
-     */
-    decidirCandidatosElectos(resultados){
-        let mayor = 0
-        const resultadosFinales = []
-        resultados.forEach(candidato => {
-            if(candidato.votos > mayor){
+    const resultadosFinales = [];
 
-                mayor = candidato.votos
-
-            }
-        })
-
-        resultados.forEach(candidato => {
-            if(candidato.votos === mayor){
-                resultadosFinales.push({id: candidato.id,  resultado: 1, votos: candidato.votos})
-            } else {
-                resultadosFinales.push({id: candidato.id, resultado: 0, votos: candidato.votos})
-            }
-        })
-
-        return resultadosFinales
+    for (let i = 0; i < len; i++) {
+      if (i < candidatosElegidos) {
+        resultadosFinales.push({
+          id: resultados[i].id,
+          resultado: 1,
+          votos: resultados[i].votos,
+        });
+      } else {
+        resultadosFinales.push({
+          id: resultados[i].id,
+          resultado: 0,
+          votos: resultados[i].votos,
+        });
+      }
     }
 
-    /** 
-     * Cuenta las veces que se repite un valor en un arreglo
-     * @param arreglo {number array}
-     * @param valor {number}, valor al que se le contará la frecuencia en el arreglo
-     * @return frecuencia {number}
-     */
-    obtenerFrecuencia(arreglo, valor) {
-        let frecuencia = 0
-        for (let i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] === valor) {
-                frecuencia += 1
-            }
-        }
-        return frecuencia
+    return resultadosFinales;
+  }
+
+  /**
+   * Cuenta las veces que se repite un valor en un arreglo
+   * @param arreglo {number array}
+   * @param valor {number}, valor al que se le contará la frecuencia en el arreglo
+   * @return frecuencia {number}
+   */
+  obtenerFrecuencia(arreglo, valor) {
+    let frecuencia = 0;
+    for (let i = 0; i < arreglo.length; i++) {
+      if (arreglo[i] === valor) {
+        frecuencia += 1;
+      }
     }
+    return frecuencia;
+  }
 }
 
 module.exports = Votacion;
--- test/Votacion.test.js
+++ test/Votacion.test.js
@@ -1,13 +1,54 @@
 /* eslint-disable no-undef */
-const Votacion = require('../server/services/Votacion')
-const vota = new Votacion()
+const Votacion = require("../server/services/Votacion");
+const vota = new Votacion();
 
-test('Conteo de votos debe ser ME1 = 2, ME2 = 1, ME3 = 1', () => {
-    const votos = ['ME1', 'ME1', 'ME2', 'ME3']
-    expect(vota.contarVotos(votos)).toEqual(expect.arrayContaining([{ id: 'ME1', resultado: 1, votos: 2 }, { id: 'ME2', resultado: 0, votos: 1 }, { id: 'ME3', resultado: 0, votos: 1 }]))
-})
+test("Conteo de votos debe ser ME1 = 2, ME2 = 1, ME3 = 1 y los 3 candidatos quedan electos", () => {
+  const votos = ["ME1", "ME1", "ME2", "ME3"];
+  expect(vota.contarVotos(votos)).toEqual(
+    expect.arrayContaining([
+      { id: "ME1", resultado: 1, votos: 2 },
+      { id: "ME2", resultado: 1, votos: 1 },
+      { id: "ME3", resultado: 1, votos: 1 },
+    ])
+  );
+});
 
-test('Conteo de frecuencia en arreglo debe ser 3', () => {
-    const arreglo = ['a', 'b', 'c', 'a', 'c', 'b', 'c', 'a', 'b']
-    expect(vota.obtenerFrecuencia(arreglo, 'a')).toBe(3)
-})
+test("Conteo de frecuencia en arreglo debe ser 3", () => {
+  const arreglo = ["a", "b", "c", "a", "c", "b", "c", "a", "b"];
+  expect(vota.obtenerFrecuencia(arreglo, "a")).toBe(3);
+});
+
+test("Elección de los 8 candidatos con más votos obtenidos", () => {
+  const candidatos = [];
+  candidatos.push({ id: "ME1", votos: 20 });
+  candidatos.push({ id: "ME2", votos: 30 });
+  candidatos.push({ id: "ME3", votos: 3 });
+  candidatos.push({ id: "ME4", votos: 40 });
+  candidatos.push({ id: "ME5", votos: 32 });
+  candidatos.push({ id: "ME6", votos: 1 });
+  candidatos.push({ id: "ME7", votos: 90 });
+  candidatos.push({ id: "ME8", votos: 75 });
+  candidatos.push({ id: "ME9", votos: 11 });
+  candidatos.push({ id: "ME10", votos: 27 });
+  candidatos.push({ id: "ME11", votos: 93 });
+  candidatos.push({ id: "ME12", votos: 7 });
+
+  const resultados = [
+    { id: "ME11", resultado: 1, votos: 93 },
+    { id: "ME7", resultado: 1, votos: 90 },
+    { id: "ME8", resultado: 1, votos: 75 },
+    { id: "ME4", resultado: 1, votos: 40 },
+    { id: "ME5", resultado: 1, votos: 32 },
+    { id: "ME2", resultado: 1, votos: 30 },
+    { id: "ME10", resultado: 1, votos: 27 },
+    { id: "ME1", resultado: 1, votos: 20 },
+    { id: "ME9", resultado: 0, votos: 11 },
+    { id: "ME12", resultado: 0, votos: 7 },
+    { id: "ME3", resultado: 0, votos: 3 },
+    { id: "ME6", resultado: 0, votos: 1 },
+  ];
+
+  expect(vota.decidirCandidatosElectos(candidatos)).toEqual(
+    expect.arrayContaining(resultados)
+  );
+});
